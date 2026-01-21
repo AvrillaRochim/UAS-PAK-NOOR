20f2a3c87c8561d7fbff056ad91c4e00109bc2d6
AvrillaRochim <avrilr9557@gmail.com>
2026-01-21 10:32:48 +0700

    UAS_PAK_NOOR

--- UAS_PAK_NOOR.py
+++ UAS_PAK_NOOR.py
@@ -1,82 +1,34 @@
 import random
 
 print("==== GAME TEBAK ANGKA ====")
-nama = input("Masukan nama kamu: ")
 
-print("\nLEVEL 1 : PEMANASAN")
 print("Rules")
 print("1. Tebak angka dari 1 sampai 100")
 print("2. Kesempatan 15 kali")
 print("3. Tidak ada batas waktu")
-print("4. Setiap tebakan akan diberi petunjuk apakah angka terlalu ke atas atau terlalu ke bawah")
-
-batas_bawah = 1   #
-batas_atas  = 100 #
+print("4. Setiap tebakan akan diberi petunjuk apakah angka terlalu ke besar atau terlalu ke kecil")
 kesempatan  = 15  #
 
-skor = 2000
-
-angka = random.randint(batas_bawah, batas_atas)
+angka = random.randint(1, 100)
 
 
 print("\nSaya sudah memilih angka antara 1 sampai 100")
 
 for i in range(kesempatan):
     print(f"\nKesempatan ke-{i+1}")
-    print(f"skor saat ini: {skor}")
 
 
     tebakan = int(input("Masukkan tebakan kamu: "))
 
     if tebakan > angka:
         print("Angkaku lebih ke kecil")
-        skor -= 5
     elif tebakan < angka:
         print("Angkaku lebih ke besar")
-        skor -= 5
     else:
-        print(f"Selamat {nama}, tebakan kamu BENAR!")
-        skor += 20
+        print(f"Selamat, tebakan kamu BENAR!")
         break
 else:
-    print("Kesempatan habis!")
+    print("\nKesempatan habis!")
     print(f"Angka yang benar adalah: {angka}")
-    print("\n=== GAME SELESAI ===")
-    print(f"Nama : {nama}")
-    print(f"skor akhir : {skor}")
+    print("=== GAME OVER ===")
     exit()
-
-# ================= LEVEL 2 =================
-print("\nLEVEL 2: SERIUS")
-print("Rules sama seperti sebelumnya")
-
-
-batas_bawah = 1
-batas_atas  = 500
-kesempatan  = 15
-
-angka = random.randint(batas_bawah, batas_atas)
-
-for i in range(kesempatan):
-    print(f"\nKesempatan ke-{i+1}")
-    print(f"Skor saat ini: {skor}")
-
-    tebakan = int(input("Masukkan tebakan kamu: "))
-
-    if tebakan > angka:
-        print("Angkaku lebih kecil")
-        skor -= 10
-    elif tebakan < angka:
-        print("Angkaku lebih ke besar")
-        skor -= 10
-    else:
-        print(f"Selamat {nama}, tebakan kamu BENAR!")
-        skor += 50
-        break
-else:
-    print(" Kesempatan habis!")
-    print(f"Angka yang benar adalah: {angka}")
-
-print("\n=== GAME SELESAI ===")
-print(f"Nama : {nama}")
-print(f"Skor akhir : {skor}")
